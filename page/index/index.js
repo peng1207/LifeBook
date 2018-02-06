f93cd083aecc0d68b7dc4a588ebf1387956b3d3b
huangshupeng <825844231@qq.com>
2018-02-06 10:59:13 +0800

    modify index

--- page/index/index.js
+++ page/index/index.js
@@ -65,9 +65,9 @@ Page({
   },
   buttonOnClick:function(e){
     this.setData({
-      title:"button"
+      title:"button",
+      listArray: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]
     });
   console.log("click ",e);
-
   }
 })
